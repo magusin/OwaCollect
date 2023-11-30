417d8c22ebd83b9adbfe2162252b2a794fe21603
Hugo JACQUES <fanatsy4@gmail.com>
2023-11-30 23:49:37 +0100

    add secret for production

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import TwitchProvider from "next-auth/providers/twitch";
 
 export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     TwitchProvider({
       clientId: process.env.TWITCH_CLIENT_ID,
