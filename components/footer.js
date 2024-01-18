37efcf7bc9c3c507c5fe7ab598ab8ddd786163a6
Hugo JACQUES <fanatsy4@gmail.com>
2024-01-19 00:02:29 +0100

    fix darkmode footer refresh

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import { useDarkMode } from "@/contexts/darkModeContext";
-
+import { useSession } from "next-auth/react";
 export default function Footer() {
     const { darkMode } = useDarkMode();
+    const { data: session, status } = useSession();
 
     return (
+        <>
+        {session ? (
         <footer className={`text-center md:flex justify-around items-center min-w-full w-full ${darkMode ? 'text-black' : 'text-white'}`} style={{ backgroundColor:'#009900', minHeight: '50px' }}>
             <p>© 2024 - OwaCollect</p>
             <nav className="md:flex space-x-4 md:space-x-8 xl:space-x-16">
@@ -12,5 +15,9 @@ export default function Footer() {
                 <Link href="/contact" className="hover:text-gray-300">Contact</Link>
             </nav>
         </footer>
+        ) : (
+        null
+        )}
+        </>
     );
-    }
+}
