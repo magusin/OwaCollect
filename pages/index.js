da35f15d6917d6b7c6be4eda46eafce798e8c09d
Hugo JACQUES <fanatsy4@gmail.com>
2024-04-07 21:38:41 +0200

    start corr maj

--- pages/index.js
+++ pages/index.js
@@ -148,7 +148,7 @@ export default function Login() {
         <Header />
         <div className="flex flex-col h-full w-full justify-center items-center" style={{ background: 'radial-gradient(circle, #CCCCCC, #0f171b)' }}>
           <p>Maintenance en cours ...</p>
-          <p>Ca reviens soon soon</p>
+          <p>Correction 07/04/24</p>
           
         </div>
       </div>
