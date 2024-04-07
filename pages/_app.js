2d8ccdb75dc4bb1cc3ffb454fd2dd6dfa5942288
Hugo JACQUES <fanatsy4@gmail.com>
2024-04-07 19:35:38 +0200

    add SpeedInsights vercel

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import '@/styles/globals.css'
 import { SessionProvider } from "next-auth/react";
 import GoogleAnalytics from "C/googleAnalytics";
 import { DarkModeProvider } from '@/contexts/darkModeContext';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }) {
   
   
   return (
     <SessionProvider session={pageProps.session}>
+      <SpeedInsights />
       <GoogleAnalytics trackingId="G-VQ455GC1GK" />
       <DarkModeProvider>
       <Component {...pageProps} />
