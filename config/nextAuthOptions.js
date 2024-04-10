10f9b92aed15bb4c3127bf5e909c73825168c7f7
Hugo JACQUES <fanatsy4@gmail.com>
2024-04-10 22:08:52 +0200

    redeploy

--- config/nextAuthOptions.js
+++ config/nextAuthOptions.js
@@ -1,47 +1,47 @@
-// nextAuthOptions.js
+import NextAuth from "next-auth";
 import TwitchProvider from "next-auth/providers/twitch";
+import jwt from 'jsonwebtoken';
 import axios from "axios";
 
-const nextAuthOptions = {
+export default NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     TwitchProvider({
       clientId: process.env.TWITCH_CLIENT_ID,
       clientSecret: process.env.TWITCH_CLIENT_SECRET,
       authorization: {
-        // necessary scope permissions
         params: {
-          scope: 'openid user:read:email user:read:subscriptions'
+          scope: 'openid user:read:email'
         },
       },
     }),
   ],
   callbacks: {
     async jwt({ token, account, profile, user }) {
-      // Persist the OAuth access_token and or the user id to the token right after signin     
-      if (account && user) {
-        token.id = account.providerAccountId;
-        const userPayload = {
-          id: token.id,
-          name: token.name,
-          email: token.email,
-          image: token.picture
-        };
-        // const expiresIn = Math.floor(account.expires_at / 1000)
-        const expiresIn = Date.now() + 3600000;
-        const customJwt = jwt.sign(userPayload, process.env.JWT_SECRET, { expiresIn });
-        token.customJwt = customJwt;
-        token.accessToken = account.access_token;
-        token.refreshToken = account.refresh_token;
-        token.accessTokenExpires = expiresIn;
-      }
+        // Persist the OAuth access_token and or the user id to the token right after signin     
+        if (account && user) {
+          token.id = account.providerAccountId;
+          const userPayload = {
+            id: token.id,
+            name: token.name,
+            email: token.email,
+            image: token.picture
+          };
+          // const expiresIn = Math.floor(account.expires_at / 1000)
+          const expiresIn = Date.now() + 3600000;
+          const customJwt = jwt.sign(userPayload, process.env.JWT_SECRET, { expiresIn });
+          token.customJwt = customJwt;
+          token.accessToken = account.access_token;
+          token.refreshToken = account.refresh_token;
+          token.accessTokenExpires = expiresIn;
+        }
 
-      if (Date.now() < token.accessTokenExpires) {
-        return token
-      }
-      // Access token has expired, try to update it
-      return refreshAccessToken(token)
-    },
+        if (Date.now() < token.accessTokenExpires) {
+          return token
+        }
+        // Access token has expired, try to update it
+        return refreshAccessToken(token)
+      },
     async session({ session, token, user }) {
       // Send properties to the client
       // session.accessToken = token.accessToken
@@ -50,6 +50,4 @@ const nextAuthOptions = {
       return session
     }
   }
-};
-
-export default nextAuthOptions;
+});
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,37 +11,37 @@ export default NextAuth({
       clientSecret: process.env.TWITCH_CLIENT_SECRET,
       authorization: {
         params: {
-          scope: 'openid user:read:email user:read:subscriptions'
+          scope: 'openid user:read:email'
         },
       },
     }),
   ],
   callbacks: {
     async jwt({ token, account, profile, user }) {
-      // Persist the OAuth access_token and or the user id to the token right after signin     
-      if (account && user) {
-        token.id = account.providerAccountId;
-        const userPayload = {
-          id: token.id,
-          name: token.name,
-          email: token.email,
-          image: token.picture
-        };
-        // const expiresIn = Math.floor(account.expires_at / 1000)
-        const expiresIn = Date.now() + 3600000;
-        const customJwt = jwt.sign(userPayload, process.env.JWT_SECRET, { expiresIn });
-        token.customJwt = customJwt;
-        token.accessToken = account.access_token;
-        token.refreshToken = account.refresh_token;
-        token.accessTokenExpires = expiresIn;
-      }
+        // Persist the OAuth access_token and or the user id to the token right after signin     
+        if (account && user) {
+          token.id = account.providerAccountId;
+          const userPayload = {
+            id: token.id,
+            name: token.name,
+            email: token.email,
+            image: token.picture
+          };
+          // const expiresIn = Math.floor(account.expires_at / 1000)
+          const expiresIn = Date.now() + 3600000;
+          const customJwt = jwt.sign(userPayload, process.env.JWT_SECRET, { expiresIn });
+          token.customJwt = customJwt;
+          token.accessToken = account.access_token;
+          token.refreshToken = account.refresh_token;
+          token.accessTokenExpires = expiresIn;
+        }
 
-      if (Date.now() < token.accessTokenExpires) {
-        return token
-      }
-      // Access token has expired, try to update it
-      return refreshAccessToken(token)
-    },
+        if (Date.now() < token.accessTokenExpires) {
+          return token
+        }
+        // Access token has expired, try to update it
+        return refreshAccessToken(token)
+      },
     async session({ session, token, user }) {
       // Send properties to the client
       // session.accessToken = token.accessToken
