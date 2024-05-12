1d17ff4a286f129034544ccfc03b072a536c80b8
HJ <fanatsy>
2024-05-12 17:52:08 +0200

    add playlist miniature, component playlist

--- components/playlist.js
+++ components/playlist.js
@@ -1,23 +1,66 @@
 import React, { useState } from 'react';
 import TwitchPlayer from 'C/twitchPlayer';
+import VideoItem from 'C/videoItem';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import SwiperCore, { Navigation } from 'swiper';
+// const Playlist = ({ videos }) => {
+//   const [currentVideo, setCurrentVideo] = useState(videos[0]);
 
-const Playlist = ({ videos }) => {
-  const [currentVideo, setCurrentVideo] = useState(videos[0]);
+//   return (
+//     <div className="flex">
+//       <div className="flex-grow">
+//         {/* Utilisez TwitchPlayer */}
+//         <TwitchPlayer videoId={currentVideo.src} collection={currentVideo.collection} />
+//       </div>
+//       <div className="w-1/4 bg-gray-800 h-screen overflow-auto">
+//         {videos.map(video => (
+//           <div key={video.id} className="p-4 hover:bg-gray-700 cursor-pointer" onClick={() => setCurrentVideo(video)}>
+//             {video.title}
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
 
+// Installer les modules Swiper nécessaires
+SwiperCore.use([Navigation]);
+
+const Playlist = ({ playlist, index }) => {
+  // Des identifiants uniques pour chaque instance de Swiper
+  // const nextButtonRef = `swiper-button-next-${index}`;
+  // const prevButtonRef = `swiper-button-prev-${index}`;
   return (
-    <div className="flex">
-      <div className="flex-grow">
-        {/* Utilisez TwitchPlayer au lieu de VideoPlayer */}
-        <TwitchPlayer videoId={currentVideo.src} collection={currentVideo.collection} />
+      <div className="mb-8 relative">
+          <h2 className="text-xl font-bold mb-4">{playlist.title}</h2>
+          <Swiper
+              spaceBetween={20}
+              slidesPerView={3}
+            //   navigation={{
+            //     nextEl: `.${nextButtonRef}`,
+            //     prevEl: `.${prevButtonRef}`,
+            // }}
+              loop={true}
+              observer={true}
+              observeParents={true}
+              parallax={true}
+          >
+              {playlist.videos.map(video => (
+                  <SwiperSlide key={video.id}>
+                      <a href={video.url} target="_blank" rel="noopener noreferrer">
+                          <div className="aspect-w-16 aspect-h-9">
+                              <img src={video.thumbnail} alt={video.title} className="object-cover" />
+                          </div>
+                          <p className="text-center mt-2">{video.title}</p>
+                      </a>
+                  </SwiperSlide>
+              ))}
+          </Swiper>
+          {/* <div className={`swiper-button-next ${nextButtonRef}`}></div> */}
+            {/* <div className={`swiper-button-prev ${prevButtonRef}`}></div> */}
       </div>
-      <div className="w-1/4 bg-gray-800 h-screen overflow-auto">
-        {videos.map(video => (
-          <div key={video.id} className="p-4 hover:bg-gray-700 cursor-pointer" onClick={() => setCurrentVideo(video)}>
-            {video.title}
-          </div>
-        ))}
-      </div>
-    </div>
   );
 };
 
--- pages/challenges/index.js
+++ pages/challenges/index.js
@@ -1,15 +1,50 @@
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
 import React from 'react';
+import Playlist from 'C/playlist';
+import PlaylistsContainer from 'C/playlistContainer';
 
 const DynamicPlaylist = dynamic(() => import('C/playlist'), {
   suspense: true,
   ssr: false  // Ensure it's only client-side rendered if it involves complex state interactions
 });
 
-const videos = [
-    { id: 1, title: "Marathon Souls ", src: "2107182379", collection: "qCqr4xD3wRdumg" },
-    { id: 2, title: "Playlist Lord of the Fallen", src: "1959635843", collection: "h-7MlnY2kRc5-g" }
+// const videos = [
+//     { id: 1, title: "Marathon Souls ", src: "2107182379", collection: "qCqr4xD3wRdumg" },
+//     { id: 2, title: "Playlist Lord of the Fallen", src: "1959635843", collection: "h-7MlnY2kRc5-g" }
+// ];
+
+const playlists = [
+  {
+      title: "Playlist 1",
+      videos: [
+          { id: 1, title: "Ep.1❄️MARATHON SOULS ALL BOSSES❄️Les 4 jeux à la ZWEIHANDER", thumbnail: "https://static-cdn.jtvnw.net/cf_vods/dgeft87wbj63p/0c40672f3199c1366590_owarida_11654714028_5539955234//thumb/thumb2107182379-320x180.jpg", videoId: "2107182379", collection: "qCqr4xD3wRdumg", url: "https://www.twitch.tv/videos/2107182379?collection=qCqr4xD3wRdumg" },
+          { id: 2, title: "Ep.2❄️MARATHON SOULS ALL BOSSES❄️Les 4 jeux à la ZWEIHANDER", thumbnail: "https://static-cdn.jtvnw.net/cf_vods/d1m7jfoe9zdc1j/0d311347ceed1689cfa4_owarida_76406526343_4564151795//thumb/thumb2107182876-320x180.jpg", videoId: "2107182876", collection: "qCqr4xD3wRdumg", url: "https://www.twitch.tv/videos/2107182876?collection=qCqr4xD3wRdumg" },
+          { id: 3, title: "Ep.3❄️MARATHON SOULS ALL BOSSES❄️Les 4 jeux à la ZWEIHANDER", thumbnail: "https://static-cdn.jtvnw.net/cf_vods/dgeft87wbj63p/fd0813b66a9e67529f28_owarida_13316461860_4665390885//thumb/thumb2107183284-320x180.jpg", videoId: "2107183284", collection: "qCqr4xD3wRdumg", url: "https://www.twitch.tv/videos/2107183284?collection=qCqr4xD3wRdumg" },
+          { id: 4, title: "Video 4", thumbnail: "https://static-cdn.jtvnw.net/cf_vods/dgeft87wbj63p/0c40672f3199c1366590_owarida_11654714028_5539955234//thumb/thumb2107182379-320x180.jpg", videoId: "2107182379", collection: "qCqr4xD3wRdumg", url: "https://www.twitch.tv/videos/2107182379?collection=qCqr4xD3wRdumg" }
+      ]
+  },
+  {
+    title: "Playlist 2",
+    videos: [
+        { id: 1, title: "Ep.1❄️MARATHON SOULS ALL BOSSES❄️Les 4 jeux à la ZWEIHANDER", thumbnail: "https://static-cdn.jtvnw.net/cf_vods/dgeft87wbj63p/0c40672f3199c1366590_owarida_11654714028_5539955234//thumb/thumb2107182379-320x180.jpg", videoId: "2107182379", collection: "qCqr4xD3wRdumg", url: "https://www.twitch.tv/videos/2107182379?collection=qCqr4xD3wRdumg" },
+        { id: 2, title: "Ep.2❄️MARATHON SOULS ALL BOSSES❄️Les 4 jeux à la ZWEIHANDER", thumbnail: "https://static-cdn.jtvnw.net/cf_vods/d1m7jfoe9zdc1j/0d311347ceed1689cfa4_owarida_76406526343_4564151795//thumb/thumb2107182876-320x180.jpg", videoId: "2107182876", collection: "qCqr4xD3wRdumg", url: "https://www.twitch.tv/videos/2107182876?collection=qCqr4xD3wRdumg" },
+        { id: 3, title: "Ep.3❄️MARATHON SOULS ALL BOSSES❄️Les 4 jeux à la ZWEIHANDER", thumbnail: "https://static-cdn.jtvnw.net/cf_vods/dgeft87wbj63p/fd0813b66a9e67529f28_owarida_13316461860_4665390885//thumb/thumb2107183284-320x180.jpg", videoId: "2107183284", collection: "qCqr4xD3wRdumg", url: "https://www.twitch.tv/videos/2107183284?collection=qCqr4xD3wRdumg" },
+        { id: 4, title: "Video 4", thumbnail: "https://static-cdn.jtvnw.net/cf_vods/dgeft87wbj63p/0c40672f3199c1366590_owarida_11654714028_5539955234//thumb/thumb2107182379-320x180.jpg", videoId: "2107182379", collection: "qCqr4xD3wRdumg", url: "https://www.twitch.tv/videos/2107182379?collection=qCqr4xD3wRdumg" }
+    ]
+},
+{
+  title: "Playlist 3",
+  videos: [
+      { id: 1, title: "Ep.1❄️MARATHON SOULS ALL BOSSES❄️Les 4 jeux à la ZWEIHANDER", thumbnail: "https://static-cdn.jtvnw.net/cf_vods/dgeft87wbj63p/0c40672f3199c1366590_owarida_11654714028_5539955234//thumb/thumb2107182379-320x180.jpg", videoId: "2107182379", collection: "qCqr4xD3wRdumg", url: "https://www.twitch.tv/videos/2107182379?collection=qCqr4xD3wRdumg" },
+      { id: 2, title: "Ep.2❄️MARATHON SOULS ALL BOSSES❄️Les 4 jeux à la ZWEIHANDER", thumbnail: "https://static-cdn.jtvnw.net/cf_vods/d1m7jfoe9zdc1j/0d311347ceed1689cfa4_owarida_76406526343_4564151795//thumb/thumb2107182876-320x180.jpg", videoId: "2107182876", collection: "qCqr4xD3wRdumg", url: "https://www.twitch.tv/videos/2107182876?collection=qCqr4xD3wRdumg" },
+      { id: 3, title: "Ep.3❄️MARATHON SOULS ALL BOSSES❄️Les 4 jeux à la ZWEIHANDER", thumbnail: "https://static-cdn.jtvnw.net/cf_vods/dgeft87wbj63p/fd0813b66a9e67529f28_owarida_13316461860_4665390885//thumb/thumb2107183284-320x180.jpg", videoId: "2107183284", collection: "qCqr4xD3wRdumg", url: "https://www.twitch.tv/videos/2107183284?collection=qCqr4xD3wRdumg" },
+      { id: 4, title: "Video 4", thumbnail: "https://static-cdn.jtvnw.net/cf_vods/dgeft87wbj63p/0c40672f3199c1366590_owarida_11654714028_5539955234//thumb/thumb2107182379-320x180.jpg", videoId: "2107182379", collection: "qCqr4xD3wRdumg", url: "https://www.twitch.tv/videos/2107182379?collection=qCqr4xD3wRdumg" },
+      { id: 5, title: "Ep.1❄️MARATHON SOULS ALL BOSSES❄️Les 4 jeux à la ZWEIHANDER", thumbnail: "https://static-cdn.jtvnw.net/cf_vods/dgeft87wbj63p/0c40672f3199c1366590_owarida_11654714028_5539955234//thumb/thumb2107182379-320x180.jpg", videoId: "2107182379", collection: "qCqr4xD3wRdumg", url: "https://www.twitch.tv/videos/2107182379?collection=qCqr4xD3wRdumg" },
+      { id: 6, title: "Ep.2❄️MARATHON SOULS ALL BOSSES❄️Les 4 jeux à la ZWEIHANDER", thumbnail: "https://static-cdn.jtvnw.net/cf_vods/d1m7jfoe9zdc1j/0d311347ceed1689cfa4_owarida_76406526343_4564151795//thumb/thumb2107182876-320x180.jpg", videoId: "2107182876", collection: "qCqr4xD3wRdumg", url: "https://www.twitch.tv/videos/2107182876?collection=qCqr4xD3wRdumg" },
+      
+  ]
+}
 ];
 
 function HeadView() {
@@ -24,14 +59,20 @@ function HeadView() {
 }
 
 export default function Challenges() {
+  // return (
+  //   <>
+  //     <HeadView />
+  //     <div className="container mx-auto px-4 text-white">
+  //       <React.Suspense fallback={<div>Loading...</div>}>
+  //         <DynamicPlaylist videos={videos} />
+  //       </React.Suspense>
+  //     </div>
+  //   </>
+  // );
   return (
-    <>
-      <HeadView />
-      <div className="container mx-auto px-4 text-white">
-        <React.Suspense fallback={<div>Loading...</div>}>
-          <DynamicPlaylist videos={videos} />
-        </React.Suspense>
-      </div>
-    </>
-  );
+    <div className="container mx-auto px-4 py-8">
+        <h1 className="text-xl font-bold text-center mb-4">Video Playlists</h1>
+        <PlaylistsContainer playlists={playlists} />
+    </div>
+);
 }
