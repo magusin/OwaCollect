d86cef9eeab8b17145606a1575176ac0fb2d0183
HJ <fanatsy>
2024-05-24 22:21:39 +0200

    fix dimension img 404

--- pages/404.js
+++ pages/404.js
@@ -102,31 +102,44 @@ export default function Alley() {
 
     return (
         <>
-            <HeadView />
-        <div className="flex flex-col h-screen">
-            <Header points={points}/>
+          <HeadView />
+          <div className="flex flex-col h-screen">
+            <Header points={points} />
             <div className="relative flex-grow flex justify-center">
-            {isMobile ? (
-            <Image src="/images/404.webp" alt="404" fill priority={true} />
-            ) : (
-            <Image src="/images/404-2.webp" alt="404" fill priority={true} />
-            )}
-            <div className="absolute inset-0 flex items-center justify-center p-4 text-center">
-        <p className="mb-8 text-black text-3xl md:text-4xl shadow-lg font-bold bg-black/5">
-          Vous aussi vous vous perdez facilement comme Owa ?
-        </p>
-        </div>
-        <div className='absolute p-4 bottom-8 flex items-center text-center justify-center'>
-
-        <Link href="/"
-          className="text-lg md:text-xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Retour à l&apos;accueil
-          
-        </Link>
+              {isMobile ? (
+                <Image
+                  src="/images/404.webp"
+                  alt="404"
+                  layout="responsive"
+                  objectFit="contain"
+                  width={1920}
+                  height={1080}
+                  priority
+                />
+              ) : (
+                <Image
+                  src="/images/404-2.webp"
+                  alt="404"
+                  layout="responsive"
+                  objectFit="contain"
+                  width={1920}
+                  height={1080}
+                  priority
+                />
+              )}
+              <div className="absolute inset-0 flex items-center justify-center p-4 text-center">
+                <p className="mb-8 text-black text-3xl md:text-4xl shadow-lg font-bold bg-black/5">
+                  Vous aussi vous vous perdez facilement comme Owa ?
+                </p>
+              </div>
+              <div className="absolute p-4 bottom-8 flex items-center text-center justify-center">
+                <Link href="/" className="text-lg md:text-xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                  Retour à l&apos;accueil
+                </Link>
               </div>
             </div>
             <Footer />
-        </div>
+          </div>
         </>
-    );
+      );
 }
